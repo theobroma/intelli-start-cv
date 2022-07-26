58a0586a029fcd62ee5eedbe167f7aa6e84d33e4
Alexandr Siryj <theobroma222@gmail.com>
2022-07-26 03:21:34 +0300

    rm tailwind

--- theobroma-cv/src/main.tsx
+++ theobroma-cv/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import 'tailwindcss/tailwind.css';
 import './index.css';
 
 import AppContainer from '@routes/AppContainer';
